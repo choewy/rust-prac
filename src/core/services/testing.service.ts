79efd9e24c18b03fd6bbd288c39c22f32e6aebdf
choewy <choewy32@gmail.com>
2022-08-27 22:52:54 +0900

    [feat] initialize tables & success log

--- src/core/services/testing.service.ts
+++ src/core/services/testing.service.ts
@@ -1,26 +1,50 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { LogDirs, TestFunc, TestFuncObject } from './types';
+import { TestFuncObject, TestTypes } from './types';
 import { getArgumentNames } from '@/core/helpers';
 import { LogService } from './log.service';
+import { AdminRepositories, RowDataPacket } from '../types';
 
 @Injectable()
 export class TestingService {
   private readonly logger = new Logger();
 
+  private async initializeTables(dataSource: DataSource) {
+    const tableNamePackets: RowDataPacket[] = await dataSource.query(
+      'SHOW TABLES;',
+    );
+
+    const tableNames = tableNamePackets
+      .map((packet) => {
+        return Object.values(packet).map((tableName) => tableName)[0];
+      })
+      .sort((current, next) => next.length - current.length);
+
+    for (const tableName of tableNames) {
+      const query = `DELETE FROM ${tableName};`;
+      await dataSource.query(query);
+    }
+  }
+
   constructor(private readonly logService: LogService) {}
 
   async testing(
-    type: keyof LogDirs,
+    type: TestTypes,
     dataSource: DataSource,
-    repositories: object,
+    repositories: AdminRepositories,
     testObject: TestFuncObject,
   ): Promise<number> {
+    await this.logService.makeLogDirs();
+    await this.initializeTables(dataSource);
+
     let errorCount = 0;
     const targets = Object.entries(testObject);
 
-    for (const [name, func] of targets) {
+    for (const i in targets) {
+      const row = Number(i);
+      const [name, func] = targets[row];
       const time = new Date().toLocaleString();
+      const message = `${time} - (${row + 1})${type}(${name})`;
 
       const queryRunner = dataSource.createQueryRunner();
       await queryRunner.connect();
@@ -28,13 +52,17 @@ export class TestingService {
 
       try {
         const args = getArgumentNames(func).map((arg) => repositories[arg]);
-        await func(...args);
+        const data = await func(...args);
         await queryRunner.commitTransaction();
-        this.logger.log(`[success] ${time} - ${type}(${name})`);
+        await this.logService.writeSuccessLog(type, row + 1, name, time, data);
+        this.logger.log(`[success] ${message}`);
       } catch (error) {
         await queryRunner.rollbackTransaction();
-        this.logService.writeLogfile(type, name, time, error);
-        this.logger.error(`[error] ${time} - ${type}(${name})`);
+        await this.logService.writeErrorLog(type, row + 1, name, time, {
+          ...error,
+          stack: error.stack,
+        });
+        this.logger.error(`[error] ${message}`);
         errorCount += 1;
       } finally {
         await queryRunner.release();
--- src/core/services/types.ts
+++ src/core/services/types.ts
@@ -1,11 +1,10 @@
 export type LogDirs = {
-  root: string;
   admin: string;
   service: string;
 };
 
+export type TestTypes = keyof LogDirs;
 export type TestFunc = (...args: any[]) => any;
-
 export type TestFuncObject = {
   [key: string]: TestFunc;
 };
