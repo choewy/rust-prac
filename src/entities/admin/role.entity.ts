0779307dceec0ce985c1be6d061c8c862c011f39
choewy <choewy32@gmail.com>
2022-08-28 18:10:05 +0900

    [refactor] delete jointables

--- src/entities/admin/role.entity.ts
+++ src/entities/admin/role.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinTable,
-  ManyToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { TimeStamp } from '../timestamp';
 import { Manager } from './manager.entity';
 
